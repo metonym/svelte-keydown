c0835d7ca2e034e090d2f8482cbb7d003e024c6d
metonym <ericyl.us@gmail.com>
2024-03-14 18:38:36 -0700

    build!: drop bundled support (#20)

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,3 @@
-import resolve from "@rollup/plugin-node-resolve";
-import svelte from "rollup-plugin-svelte";
 import svelteReadme from "svelte-readme";
-import pkg from "./package.json";
 
-const BUNDLE = process.env.BUNDLE === "true";
-
-export default () => {
-  if (!BUNDLE) return svelteReadme();
-
-  return ["es", "umd"].map((format) => {
-    const UMD = format === "umd";
-
-    return {
-      input: pkg.svelte,
-      output: {
-        format,
-        file: UMD ? pkg.main : pkg.module,
-        name: UMD ? pkg.name : undefined,
-      },
-      plugins: [svelte(), resolve()],
-    };
-  });
-};
+export default svelteReadme();
